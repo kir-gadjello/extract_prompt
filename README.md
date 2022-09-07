d37c19042344973a8297632289d54dbb522ca924
kir-gadjello <111190790+kir-gadjello@users.noreply.github.com>
2022-09-07 18:19:44 +0300

    add code for v0.3, update readme

--- README.md
+++ README.md
@@ -2,8 +2,27 @@
 
 ## Synopsys
 
-This is a standalone, self-hosted, fast tool designed to help an aspiring hacker to reverse-engineer.
-No GPU needed - the tool runs pretty fast on old CPU on a hacker's laptop. Embeddings are cached for blazingly fast execution.
+This is a standalone, self-hosted, fast tool designed to help an aspiring hacker with reverse-engineering generative art found on the Net.
+Specifically, the code uses two deep neural networks - BLIP captioning NN and CLIP image-text embedder NN to estimate the textual prompt.
+The resulting caption can be used with generative text-to-image system like Stable Diffusion to generate a similar image.
+No GPU is necessary - the tool runs pretty fast on old CPU on a hacker's laptop. Embeddings are cached for blazingly fast execution.
+
+## Usage
+
+Default parameters are geared towards quick reverse-engineering of stable diffusion prompts, thus CLIP/ViTL14 and BLIP-base are used.
+```
+python3 extract_prompt.py test_images/mecha_cyberpunk.jpg
+```
+
+```
+chmod +x extract_prompt.py
+./extract_prompt.py test_images/mecha_cyberpunk.jpg
+```
+
+BLIP-large can be used for slightly enhanced quality of the caption
+```
+python3 extract_prompt.py --caption_model=large test_images/mecha_cyberpunk.jpg
+```
 
 ## Honorable mentions
 
@@ -12,7 +31,6 @@ Inspired by:
 * and its extended fork by @WonkyGrub https://github.com/WonkyGrub/clip-interrogator-customdata
 
 ## TODO
-* examples: python3 extract_prompt.py test_images/
 * folders and image caching
 * self-hosted web ui
 * ?
---
+++ utilities.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# Prompt Extractor v0.3
+# Copyright (c) 2022 kir-gadjello
+
+import os
+import requests
+from tqdm import tqdm
+import hashlib
+import base64
+from torch import device
+
+
+def touch_model(model, args):
+    if args.device != "cpu":
+        model = model.to(device(args.device))
+    if args.device != "cpu" and args.half:
+        model = model.half()
+    model.eval()
+    return model
+
+
+def chunk(seq, size):
+    ret = []
+    t = []
+    for item in seq:
+        t.append(item)
+        if len(t) >= size:
+            ret.append(t)
+            t = []
+    if len(t) > 0:
+        ret.append(t)
+    return ret
+
+
+def make_weightlist_parser(init_weights, num=int):
+    def parse_weightlist(s):
+        if len(s) == 0:
+            return dict(**init_weights)
+
+        if s.isdigit():
+            ret = dict(**init_weights)
+            for k in ret.keys():
+                ret[k] = num(s)
+            return ret
+
+        def parse_w(ss):
+            a, b = ss.split(":")
+            return a, num(b)
+
+        ret = dict(**init_weights)
+
+        for k, v in list(map(parse_w, s.split(","))):
+            ret[k] = v
+
+        return ret
+
+    return parse_weightlist
+
+
+def download_file(url, filename):
+    """
+    Helper method handling downloading large files from `url` to `filename`. Returns a pointer to `filename`.
+    """
+    chunkSize = 4096
+    r = requests.get(url, stream=True)
+    with open(filename, "wb") as f:
+        pbar = tqdm(unit="B", total=int(r.headers["Content-Length"]))
+        for chunk in r.iter_content(chunk_size=chunkSize):
+            if chunk:
+                # filter out keep-alive new chunks
+                pbar.update(len(chunk))
+                f.write(chunk)
+    return filename
+
+
+def ensure_file(url, filename=None, dstdir="./", size=None):
+    assert filename is not None
+    fpath = os.path.join(dstdir, filename)
+    if os.path.isfile(fpath):
+        if size is None:
+            return
+
+        fsize = os.stat(fpath).st_size
+        if fsize == size:
+            return
+        elif fsize != size:
+            print(f"file size mismatch! {fsize} vs expected {size}")
+
+    print("DL:", url, "->", fpath)
+
+    download_file(url, fpath)
+
+
+def load_list(filename):
+    with open(filename, "r", encoding="utf-8", errors="replace") as f:
+        items = [line.strip() for line in f.readlines()]
+    return items
+
+
+def hash_arr(arr):
+    hashGen = hashlib.sha512()
+    for elem in arr:
+        hashGen.update(elem.encode("utf-8"))
+    return base64.urlsafe_b64encode(hashGen.digest()).decode()
+
+
+printed = {}
+
+
+def print_once(s, label, printer=print):
+    global printed
+    if not printed.get(label):
+        printer(s)
+        printed[label] = True
